ae957a95a871d02f08b524c618d1cf4dc4a40c4c
Ilya Antipov <Ilya.408@yandex.ru>
2021-05-30 15:21:52 +0300

    Изменение моделей бд

--- db/models/WorkMaterial.py
+++ db/models/WorkMaterial.py
@@ -7,7 +7,7 @@ from db.models.Work import Work
 
 # Материал для работы
 class WorkMaterial(BaseModel):
-    logistic_price = DoubleField(default=0)                             # цена за логистику
+    # logistic_price = DoubleField(default=0)                             # цена за логистику
     amount = DoubleField(default=0)                                     # количество материала
     material = ForeignKeyField(Material, backref='work_materials')      # материал
     work = ForeignKeyField(Work, backref='work_materials')              # работа
--- gui/dictionary/CityUI.py
+++ gui/dictionary/CityUI.py
@@ -2,11 +2,9 @@ from kivy.lang import Builder
 from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 from kivy.uix.scrollview import ScrollView
-from kivy.uix.textinput import TextInput
 
 from gui.custom_uix.AddRowButton import AddRowButton
 from gui.custom_uix.add_dictionary.AddRowCityPopup import AddRowCityPopup
