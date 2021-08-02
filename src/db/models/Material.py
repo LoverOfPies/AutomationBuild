92c5fca79b6a3abad5482e3144a441e8952a3ee8
Ilya Antipov <Ilya.408@yandex.ru>
2021-08-02 17:22:59 +0300

    Исправлена ошибка в модели

--- src/db/models/Material.py
+++ src/db/models/Material.py
@@ -3,6 +3,7 @@ from peewee import CharField, ForeignKeyField
 
 from src.db.models.BaseModel import BaseModel
 from src.db.models.Group import Group
+from src.db.models.Subgroup import Subgroup
 from src.db.models.Unit import Unit
 
 
@@ -11,7 +12,7 @@ class Material(BaseModel):
     name = CharField(unique=True)                           # наименование
     articul = CharField()                                   # артикул
     unit = ForeignKeyField(Unit, backref='materials')       # единицы измерения (шт, кубы и т.д.)
-    subgroup = ForeignKeyField(Group, backref='materials')  # подгруппа
+    subgroup = ForeignKeyField(Subgroup, backref='materials')  # подгруппа
 
     class Meta:
         db_table = "ab_material"
