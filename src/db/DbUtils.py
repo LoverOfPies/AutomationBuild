9038d6e35d30dc3bd96984440150da40f5b1b27e
Ilya Antipov <Ilya.408@yandex.ru>
2021-08-21 22:03:54 +0300

    Создание таблиц в БД при запуске приложения

--- src/db/DbUtils.py
+++ src/db/DbUtils.py
@@ -1,9 +1,60 @@
 # Изменить запись в бд
-from peewee import IntegrityError
+import inspect
+import sys
 
+from peewee import IntegrityError, Model
+
+from src.db.models.BaseModel import BaseModel
 from src.gui.custom_uix.ErrorPopup import ErrorPopup
 
 
+def get_models():
+    def get_submodules(module):
+        submodules = [obj for name, obj in inspect.getmembers(module) if inspect.ismodule(obj)]
+        result_tables = []
+        for submodule in submodules:
+            if has_submodules(submodule):
+                result_tables = result_tables + get_submodules(submodule)
+            else:
+                result_tables.append(submodule)
+        return result_tables
+
+    def has_submodules(module):
+        return True if len([obj for name, obj in inspect.getmembers(module) if inspect.ismodule(obj)]) > 0 else False
+
+    def get_model_classes(module):
+        return [obj for name, obj in inspect.getmembers(module)
+                if inspect.isclass(obj)
+                and obj is not BaseModel
+                and obj is not Model
+                and isinstance(obj, type(BaseModel))]
+
+    models_module = sys.modules['src.db.models']
+    modules = get_submodules(models_module)
+    models = []
+    for model_class in modules:
+        models = models + get_model_classes(model_class)
+    return set(models)
+
+
+def create_table_with_backref(table):
+    backref_tables = table._meta.model_backrefs
+    for ref in table._meta.refs:
+        if not ref.rel_model.table_exists():
+            create_table_with_backref(ref.rel_model)
+    table.create_table()
+    for backref_table in backref_tables:
+        create_table_with_backref(backref_table)
+
+
+# Создать таблицы в БД
+def init_tables():
+    first_models = [model for model in get_models() if len(model._meta.refs) < 1]
+    for model in first_models:
+        create_table_with_backref(model)
+
+
+# Изменить значение атрибута в таблице
 def change_attribute(data):
     model_class = data.get('model_class')
     id_value = data.get('id_value')
@@ -55,10 +106,12 @@ def add_multirow(data):
             model_class.insert(value).execute()
 
 
+# Проверки при добавлении записи
 def check_value(value, model_class):
     # Проверка на заполненность полей
     fields = [key for key in model_class._meta.fields]
     fields.remove('id')
+    fields.remove('uuid')
     for field in fields:
         if bool(value[0].get(field)):
             continue
