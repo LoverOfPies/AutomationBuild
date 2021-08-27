da4be8aa8b73af8403b5de37a98a4fea80a20833
Ilya Antipov <Ilya.408@yandex.ru>
2021-08-27 11:12:54 +0300

    Модели для расчёта

--- src/db/models/project/BaseVolume.py
+++ src/db/models/project/BaseVolume.py
@@ -1,13 +1,13 @@
-from peewee import CharField, ForeignKeyField
+from peewee import ForeignKeyField, DoubleField
 
 from src.db.models.BaseModel import BaseModel
 from src.db.models.base.BaseUnit import BaseUnit
-from src.db.models.Project import Project
+from src.db.models.project.Project import Project
 
 
 # Базовый объём
 class BaseVolume(BaseModel):
-    name = CharField(unique=True)
+    amount = DoubleField(unique=True)
     project = ForeignKeyField(Project, backref='base_volumes')
     base_unit = ForeignKeyField(BaseUnit, backref='base_volumes')
 
--- src/db/models/project/TechnologyEquipment.py
+++ src/db/models/project/TechnologyEquipment.py
@@ -1,14 +1,14 @@
 from peewee import ForeignKeyField
 
 from src.db.models.BaseModel import BaseModel
-from src.db.models.Equipment import Equipment
+from src.db.models.project.Equipment import Equipment
 from src.db.models.work.WorkTechnology import WorkTechnology
 
 
 # ManyToMany Технологии для данной комплектации
 class TechnologyEquipment(BaseModel):
-    technology = ForeignKeyField(WorkTechnology)
-    equipment = ForeignKeyField(Equipment)
+    technology = ForeignKeyField(WorkTechnology, backref='equipments')
+    equipment = ForeignKeyField(Equipment, backref='technologies')
 
     class Meta:
         db_table = "ab_technology_equipment"
